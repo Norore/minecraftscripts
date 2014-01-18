2b4162d8ba5961689d2d9c4cc6a5ba0b4532637c
Norore <nolwenn.lavielle@gmail.com>
2014-01-18 12:17:50 +0100

    Extraction rapide des infos du fichier JSON

--- minecraftjsonstatsserv.py
+++ minecraftjsonstatsserv.py
@@ -2,25 +2,61 @@
 
 import json
 import re
+import unicodedata
 
 data = "minecraftserver/map/stats/playerName.json"
-fic = open(data, 'r')
-jsondata = fic.read()
-fic.close()
 
-try:
-	dico = json.loads(jsondata)
-except:
-	print "data not loaded"
+class MCJson:
+	def __init__(self, data):
+		fic = open(data, 'r')
+		jsondata = fic.read()
+		fic.close()
 
-ach,stat=1,1
-for key in dico.keys():
-	if re.match(r'achievement', key):
-#		print key,dico[key]
-		ach+=1
-	if re.match(r'stat', key):
-#		print key,dico[key]
-		stat+=1
+		self.dic_ach = {}
+		self.dic_block = {"mine":[]}
+		self.dic_item = {"use":[], "crafted":[], "break":[]}
 
-print "Nombre de succes :",ach
-print "Nombre de stat :",stat
+		try:
+			dico = json.loads(jsondata)
+		except:
+			print "data not loaded"
+
+		for key in dico.keys():
+			key.encode('ascii', 'ignore')
+			a_item = re.split('\.', key)
+			if len(a_item) >= 3:
+				ident = a_item[2].encode('ascii', 'ignore')
+
+			if re.match(r'achievement', key):
+				ach = a_item[1].encode('ascii', 'ignore')
+				self.dic_ach[ach] = dico[key]
+
+			if a_item[0] == 'stat':
+				if a_item[1] == 'useItem':
+					t_item = (int(ident), dico[key])
+					self.dic_item["use"].append(t_item)
+				if a_item[1] == 'craftItem':
+					t_item = (int(ident), dico[key])
+					self.dic_item["crafted"].append(t_item)
+				if a_item[1] == 'breakItem':
+					t_item = (int(ident), dico[key])
+					self.dic_item["break"].append(t_item)
+				if a_item[1] == 'mineBlock':
+					t_item = (int(ident), dico[key])
+					self.dic_block["mine"].append(t_item)
+
+		print "Nombre de succes :", len(self.dic_ach)
+		print self.dic_ach
+		print "Objets utilises :", len(self.dic_item["use"])
+		print self.dic_item["use"]
+		print "Objets fabriques :", len(self.dic_item["crafted"])
+		print self.dic_item["crafted"]
+		print "Objets detruits :", len(self.dic_item["break"])
+		print self.dic_item["break"]
+		print "Blocs mines :", len(self.dic_block["mine"])
+		print self.dic_block["mine"]
+
+if __name__ == "__main__":
+	MCJson(data)
+
+exit(0)
