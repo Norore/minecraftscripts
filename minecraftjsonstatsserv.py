bf3078db1258e4691de0d9a4e2a1b937b28e7b9c
Norore <nolwenn.lavielle@gmail.com>
2014-01-19 12:52:49 +0100

    Ajout des blocs minés

--- minecraftjsonstatsserv.py
+++ minecraftjsonstatsserv.py
@@ -4,7 +4,7 @@ import json
 import re
 import csv
 
-data = "minecraftserver/map/stats/playerName.json"
+data = "minecraftServer/map/stats/playerName.json"
 items = "items_id.tsv"
 
 class MCJson:
@@ -12,7 +12,7 @@ class MCJson:
 		fic = open(data, 'r')
 		jsondata = fic.read()
 		fic.close()
-	
+
 		self.items = {}
 		with open(items, 'rb') as csvfile:
 			fic = csv.reader(csvfile, delimiter='\t')
@@ -61,7 +61,7 @@ class MCJson:
 		print self.dic_ach
 		print "Objets utilises :", len(self.dic_item["use"])
 		print self.dic_item["use"]
-		print "Objets fabriques :", len(self.dic_item["crafted"])
+		print "Objets fabriques :", len(self.dic_item["craft"])
 		print self.dic_item["craft"]
 		print "Objets detruits :", len(self.dic_item["break"])
 		print self.dic_item["break"]
@@ -72,31 +72,49 @@ class MCJson:
 	def get_item_table(self):
 		table = {}
 		# ordre de recuperation :
-		# [0] => use - [1] => craft - [2] => break
-		for item in self.dic_item["use"]:
-			key = self.items[item[0]]
+		# [0] => mine - [1] => use - [2] => craft - [3] => break
+		for item in self.dic_block["mine"]:
+			try:
+				key = self.items[item[0]]
+			except:
+				key = item[0]
 			if not key in table:
-				table[key] = [0, 0, 0]
+				table[key] = [0, 0, 0, 0]
 			table[key][0] = item[1]
-		for item in self.dic_item["craft"]:
-			key = self.items[item[0]]
+
+		for item in self.dic_item["use"]:
+			try:
+				key = self.items[item[0]]
+			except:
+				key = item[0]
 			if not key in table:
-				table[key] = [0, 0, 0]
+				table[key] = [0, 0, 0, 0]
 			table[key][1] = item[1]
-		for item in self.dic_item["break"]:
-			key = self.items[item[0]]
+
+		for item in self.dic_item["craft"]:
+			try:
+				key = self.items[item[0]]
+			except:
+				key = item[0]
 			if not key in table:
-				table[key] = [0, 0, 0]
+				table[key] = [0, 0, 0, 0]
 			table[key][2] = item[1]
+
+		for item in self.dic_item["break"]:
+			try:
+				key = self.items[item[0]]
+			except:
+				key = item[0]
+			if not key in table:
+				table[key] = [0, 0, 0, 0]
+			table[key][3] = item[1]
 
 		return table
 
 if __name__ == "__main__":
 	json = MCJson(data, items)
 	table_item = json.get_item_table()
-
-	print "Item\tUsed\tCrafted\tBreak"
+	
+	print "Item\tMined\tUsed\tCrafted\tBreak"
 	for item in table_item.keys():
 		print item+"\t"+ "\t".join(str(t) for t in table_item[item])
-
-exit(0)
